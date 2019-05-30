9ab48e0649fe2976ce31d170e36abb34c1ac8411
Lahasya <lahasyakr.07@gmail.com>
2019-05-31 00:17:02 +0530

    chnages in cell and board represntn

--- start.js
+++ start.js
@@ -1,13 +1,42 @@
 const express = require('express');
+
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.static('public'))
+const Board = require('./app/models/board');
 
-app.get('/', function (req, res) {
-  res.render("home/index")
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const board = new Board();
+
+const returnGameData = (res) => {
+  const data = {
+    cells: board.cells,
+    gameOver: board.gameOver,
+    score: board.unopenedCells,
+  };
+  res.send(JSON.stringify(data));
+};
+
+app.get('/', (req, res) => {
+  res.render('home/index');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/game_data', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  returnGameData(res);
+});
+
+app.put('/cell/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!board.gameOver) {
+    board.revealDiamond(id);
+  }
+
+  returnGameData(res);
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!'); /* eslint-disable-line no-console */
 });
